591ed796fd89c27ef1c6788d67bcb029ff5f2257
Matthew Grigajtis <matthew.grigajtis@gmail.com>
2019-11-10 21:52:17 -0500

    Trying to fix root path.

---
+++ voter-tools/precache-manifest.8fd1e57ef49fd192c35f8ab82ee7f812.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/voter-tools/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/voter-tools/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/voter-tools/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/voter-tools/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/voter-tools/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/voter-tools/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e3aff6c01001a3722ec0",
+    "url": "/voter-tools/bundle.cd0c9.css"
+  },
+  {
+    "revision": "e3aff6c01001a3722ec0",
+    "url": "/voter-tools/bundle.e3aff.js"
+  },
+  {
+    "revision": "b5f1aeac670cd659c71381ea109aedf0",
+    "url": "/voter-tools/index.html"
+  },
+  {
+    "revision": "11deb5f2a3e53e5b2fd3",
+    "url": "/voter-tools/polyfills.11deb.js"
+  },
+  {
+    "revision": "752cccf5f8ee960d2010",
+    "url": "/voter-tools/route-home.chunk.752cc.js"
+  },
+  {
+    "revision": "752cccf5f8ee960d2010",
+    "url": "/voter-tools/route-home.chunk.a6799.css"
+  },
+  {
+    "revision": "6c3ef6a8234ce56fbf81",
+    "url": "/voter-tools/route-profile.chunk.62c75.css"
+  },
+  {
+    "revision": "6c3ef6a8234ce56fbf81",
+    "url": "/voter-tools/route-profile.chunk.6c3ef.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/voter-tools/sw-debug.js"
+  },
+  {
+    "revision": "2f438d795af66deed8db0e19f3567d4e",
+    "url": "/voter-tools/sw-esm.js"
+  },
+  {
+    "revision": "c38b28b20cf17f262dc5b043bbd92233",
+    "url": "/voter-tools/sw.js"
+  }
+]);
