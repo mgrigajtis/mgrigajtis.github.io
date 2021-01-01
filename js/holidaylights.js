56d15de4ae0a149c777f1443167ea85bcce4c739
Matthew Grigajtis <matthew.grigajtis@gmail.com>
2021-01-01 16:16:58 -0500

    Updates to holiday plugin

--- js/holidaylights.js
+++ js/holidaylights.js
@@ -17,9 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function letItSnow() {
-    var snowDiv = document.createElement('div');
-    snowDiv.className = 'snow';
-    snowDiv.innerHTML =  `
+    var div = document.createElement('div');
+    div.className = 'snow';
+    div.innerHTML =  `
       <i class=snow_flake></i>
       <i class=snow_flake></i>
       <i class=snow_flake></i>
@@ -42,7 +42,25 @@ document.addEventListener('DOMContentLoaded', function() {
       <i class=snow_flake-1920px></i>
       <i class=snow_flake-1920px></i>`;
 
-    document.body.insertBefore(snowDiv, document.body.childNodes[0]);
+    document.body.insertBefore(div, document.body.childNodes[0]);
+  }
+
+  function leavesAreFalling() {
+    var div = document.createElement('div');
+    div.className = 'leaves';
+    div.innerHTML =  `
+      <i class=leaf></i>
+      <i class=leaf></i>
+      <i class=leaf-600px></i>
+      <i class=leaf-768px></i>
+      <i class=leaf-1024px></i>
+      <i class=leaf-1280px></i>
+      <i class=leaf-1366px></i>
+      <i class=leaf-1600px></i>
+      <i class=leaf-1800px></i>
+      <i class=leaf-1920px></i>`;
+
+    document.body.insertBefore(div, document.body.childNodes[0]);
   }
 
   // Get the current date
@@ -51,6 +69,12 @@ document.addEventListener('DOMContentLoaded', function() {
   // Get the numeric representation of the month.
   // JavaScripts starts with month 0, so we add 1
   var currentMonth = currentDate.getMonth() + 1;
+
+  // Autumn Actions
+  if (currentMonth === 9 || currentMonth === 10 || currentMonth === 11) {
+    // Leaves fall during the Autumn months
+    leavesAreFalling();
+  }
 
   // Winter Actions
   if (currentMonth === 12 || currentMonth === 1 || currentMonth === 2) {
@@ -62,4 +86,9 @@ document.addEventListener('DOMContentLoaded', function() {
       putUpChristmasLights();
     }
   }
+
+  /* For testing */
+  // letItSnow();
+  // leavesAreFalling();
+  // putUpChristmasLights();
 });
